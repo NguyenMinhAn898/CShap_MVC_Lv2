5d8c10afce49e050dff9e4012c04d4e722b8edd6
AnNguyen <54472158+NguyenMinhAn898@users.noreply.github.com>
2021-11-04 18:19:29 +0700

    Add project files.

---
+++ Cshap_Mvc/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Cshap_Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cshap_Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<BlogModel> listBlog = new List<BlogModel>();
+
+        public ActionResult Index(BlogModel blog)
+        {
+            initData();
+
+            ViewBag.blog = blog;
+            return View(listBlog);
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Fetch data
+        /// </summary>
+        private void initData()
+        {
+            if(listBlog.Count == 0)
+            {
+                for (int i = 1; i < 11; i++)
+                {
+                    listBlog.Add(new BlogModel(i, "Hom nay la thu " + i, "Giai tri " + i,"Mô tả ngắn "+i , "Mô tả chi tiết "+i , true, "Ae " + i, DateTime.Now, true));
+                }
+            }            
+        }
+
+    }
+}
---
+++ Cshap_Mvc/Models/BlogModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cshap_Mvc.Models
+{
+    public class BlogModel
+    {
+        [Required]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter title")]
+        [Display(Name ="Tiêu đề")]
+        public String Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter short description")]
+        [Display(Name = "Mô tả ngắn")]
+        public String Short_Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter description")]
+        [Display(Name = "Chi tiết")]
+        public String Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter catgory name")]
+        [Display(Name = "Loại")]
+        public String Category { get; set; }
+
+        [Required(ErrorMessage = "Please enter status")]
+        [Display(Name = "Trạng thái")]
+        public bool Status { get; set; }
+
+        [Display(Name = "Vị trí")]
+        public String Place { get; set; }
+
+        [Display(Name = "Ngày public")]
+        public DateTime Public_Date { get; set; }
+
+        public bool Is_Active { get; set; }
+
+        public BlogModel() { }
+        public BlogModel(int id , String title, string short_description , string description , string category, bool status, String place, DateTime public_date, bool is_active)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Category = category;
+            this.Short_Description = short_description;
+            this.Description = description;
+            this.Status = status;
+            this.Place = place;
+            this.Public_Date = public_date;
+            this.Is_Active = is_active;
+        }
+    }
+}
